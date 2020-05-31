cadde9f3ada1742495c39de806637f438ce51905
louiss011371 <louiss011371@hotmail.com>
2020-05-31 18:16:01 +0800

    Add simple presenter unit test

--- app/src/test/java/com/example/stocksapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/stocksapp/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.example.stocksapp
 
+import com.example.stocksapp.presenter.StockListPresenter
+import com.example.stocksapp.view.MainFragment
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +16,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @MockK
+    lateinit var presenter : StockListPresenter
+    @MockK
+    lateinit var view : MainFragment
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        presenter = StockListPresenter(view)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testOnStockListResult() {
+        every { presenter.uiAutoUpdate() } just Runs
+        view.onStockListResult()
+        verify { presenter.uiAutoUpdate() }
+    }
+
+    @After
+    fun tearDown() {
+
     }
 }
